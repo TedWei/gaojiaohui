109aba0cee965722603d17a33a868f89cd21b8bf
tedwei <youcancallmetedwei@gmail.com>
2013-03-21 21:27:03 +0800

    Initial Commit

---
+++ gaojiaohui/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gaojiaohui
+//
+//  Created by Ted on 21/3/13.
+//  Copyright (c) 2013 卫 强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gaojiaohui/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gaojiaohui
+//
+//  Created by Ted on 21/3/13.
+//  Copyright (c) 2013 卫 强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
